0265dc2fce4d5bf79bd3b9076d8030d5a353486d
Arch Desai <archdesai.ad@gmail.com>
2020-01-04 18:11:08 -0700

    minor changes

--- reduceMemory.py
+++ reduceMemory.py
@@ -1,4 +1,15 @@
 def reduce_memory(df):
+    
+    """
+    This function reduce the dataframe memory usage by converting it's type for easier handling.
+    
+    Parameters: Dataframe whose memory needs to be reduced
+    Return: Dataframe with reduced memory usage
+    """
+    
+    start_mem_usg = df.memory_usage().sum() / 1024**2 
+    print("Memory usage of properties dataframe is :",start_mem_usg," MB")
+    
     for col in df.columns:
         if df[col].dtypes in ["int64", "int32", "int16"]:
             
@@ -14,19 +25,21 @@ def reduce_memory(df):
             elif cmin > np.iinfo(np.int32).min and cmax < np.iinfo(np.int32).max:
                 df[col] = df[col].astype(np.int32)
         
-        if df[col].dtypes in ["float64", "float32", "float16"]:
+        if df[col].dtypes in ["float64", "float32"]:
             
             cmin = df[col].min()
             cmax = df[col].max()
             
-            if cmin > np.iinfo(np.float8).min and cmax < np.iinfo(np.float8).max:
-                df[col] = df[col].astype(np.float8)
-            
-            elif cmin > np.iinfo(np.float16).min and cmax < np.iinfo(np.float16).max:
+            if cmin > np.finfo(np.float16).min and cmax < np.finfo(np.float16).max:
                 df[col] = df[col].astype(np.float16)
             
-            elif cmin > np.iinfo(np.float32).min and cmax < np.iinfo(np.float32).max:
+            elif cmin > np.finfo(np.float32).min and cmax < np.finfo(np.float32).max:
                 df[col] = df[col].astype(np.float32)
-
+    
+    print("")
+    print("___MEMORY USAGE AFTER COMPLETION:___")
+    mem_usg = df.memory_usage().sum() / 1024**2 
+    print("Memory usage is: ",mem_usg," MB")
+    print("This is ",100*mem_usg/start_mem_usg,"% of the initial size")
     
     return df

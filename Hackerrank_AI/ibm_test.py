bf00fb18e6b89322a31c6605d2f316092c69e3f1
Arch Desai <archdesai.ad@gmail.com>
2020-09-16 09:09:45 -0600

    Update ibm_test.py

--- Hackerrank_AI/ibm_test.py
+++ Hackerrank_AI/ibm_test.py
@@ -58,4 +58,21 @@ def arrangingRules(rules):
     confidence_df = confidence_df.sort_values(by='Confidence', ascending = False)
     
     return list(confidence_df.Rule.values)
+
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    rules_count = int(input().strip())
+
+    rules = []
+
+    for _ in range(rules_count):
+        rules_item = input()
+        rules.append(rules_item)
+
+    result = arrangingRules(rules)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
